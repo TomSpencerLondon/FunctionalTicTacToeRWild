e20afddd171ec2d1eaba05f4619a228fa2a30799
Tom <thomas.spencer@codurance.com>
2021-02-20 22:02:04 +0000

    Add 2 more winning for top row

--- src/main/java/tictactoe/Board.java
+++ src/main/java/tictactoe/Board.java
@@ -36,7 +36,9 @@ public class Board {
   public boolean hasWinningCombination() {
     Stream<Stream<Square>> winningCombinations = of(
         of(TOP_LEFT, CENTRE_LEFT, BOTTOM_LEFT),
-        of(TOP_MIDDLE, CENTRE_MIDDLE, BOTTOM_MIDDLE)
+        of(TOP_MIDDLE, CENTRE_MIDDLE, BOTTOM_MIDDLE),
+        of(TOP_RIGHT, CENTRE_RIGHT, BOTTOM_RIGHT),
+        of(TOP_LEFT, TOP_MIDDLE, TOP_RIGHT)
     );
     return winningCombinations.anyMatch(winningCombination ->
         winningCombination.allMatch(takenSquares::contains)
