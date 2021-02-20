fb58cfe17ea414c1fb34c1519d6a1f3e96dada89
Tom <thomas.spencer@codurance.com>
2021-02-20 21:18:31 +0000

    Add static imports

--- src/test/java/tictactoe/GameShould.java
+++ src/test/java/tictactoe/GameShould.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static tictactoe.Player.O;
+import static tictactoe.Player.X;
+import static tictactoe.Status.GAME_ON;
 
 
 public class GameShould {
@@ -11,14 +14,14 @@ public class GameShould {
   @Test
   void wait_for_X_to_play_first() {
     Game game = new Game();
-    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.X));
+    assertThat(game.state()).isEqualTo(new GameState(GAME_ON, X));
   }
 
   @Test
   void wait_for_O_to_play_after_X() {
     Game game = new Game();
     game = game.play();
-    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.O));
+    assertThat(game.state()).isEqualTo(new GameState(GAME_ON, O));
   }
 
   @Test
@@ -26,6 +29,6 @@ public class GameShould {
     Game game = new Game();
     game = game.play();
     game = game.play();
-    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.X));
+    assertThat(game.state()).isEqualTo(new GameState(GAME_ON, X));
   }
 }
