ef5100dee6405c8d233d626c6f3948835d7d4492
Tom <thomas.spencer@codurance.com>
2021-02-20 19:29:18 +0000

    Add passing test for O after X

--- src/main/java/tictactoe/Game.java
+++ src/main/java/tictactoe/Game.java
@@ -1,7 +1,27 @@
 package tictactoe;
 
+import static tictactoe.Player.X;
+import static tictactoe.Status.GAME_ON;
+
 public class Game {
+  private final Status status;
+  private final Player nextUp;
+
+  public Game() {
+    status = GAME_ON;
+    nextUp = X;
+  }
+
+  public Game(Status status, Player nextUp) {
+    this.status = status;
+    this.nextUp = nextUp;
+  }
+
   public GameState state() {
-    return new GameState(Status.GAME_ON, Player.X);
+    return new GameState(status, nextUp);
+  }
+
+  public Game play() {
+    return new Game(GAME_ON, Player.O);
   }
 }
--- src/test/java/tictactoe/GameShould.java
+++ src/test/java/tictactoe/GameShould.java
@@ -7,15 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class GameShould {
-  private Game game;
 
-  @BeforeEach
-  void setUp() {
-    game = new Game();
+  @Test
+  void wait_for_X_to_play_first() {
+    Game game = new Game();
+    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.X));
   }
 
   @Test
-  void wait_for_x_to_play_first() {
-    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.X));
+  void wait_for_O_to_play_after_X() {
+    Game game = new Game();
+    game = game.play();
+    assertThat(game.state()).isEqualTo(new GameState(Status.GAME_ON, Player.O));
   }
 }
